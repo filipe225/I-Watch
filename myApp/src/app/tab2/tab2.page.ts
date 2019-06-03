6d8c73b773b95ac9fb24b8710dedbbd21f05eac4
Filipe Sousa <filipesousaprojects@gmail.com>
2019-06-03 18:19:27 +0100

    learning

--- myApp/src/app/tab2/tab2.page.ts
+++ myApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+    series: Object[];
 
-  constructor() {}
+    constructor() {
+        this.series = [
+            {
+                name: 'Breaking Bad',
+                review: 'salk wjeqwjas, dqhwpodasncaspod odj sd qwjd doasj d jowd skdkash dqwhdq bda.',
+                rating: 5
+            },
+            {
+                name: 'Better Call Saul',
+                review: '',
+                rating: 4.5
+            },
+            {
+                name: 'sons of Anarchy',
+                review: 'ksadnq doqwjeoqw o eqwopeuqwo uou ashd hqwih qdh sj dbqiwhiqheq.',
+                rating: 3.5
+            }
+        ];
+    }
 
 }
